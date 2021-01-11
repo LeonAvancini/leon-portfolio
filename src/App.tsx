a544d72875697ec06ed4e4b6e54411bb26cd0272
LeonAvancini <avancinileon@gmail.com>
2021-01-11 19:08:22 -0300

    web: first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #734150;
+  color: white;
+`;
+const App = () => {
+  return <Container></Container>;
+};
 
 export default App;
